a53fa45a4cf4e313e13f198ce1dcd0ed7904aa74
harshitverma2103 <harshitverma2103@gmail.com>
2024-06-27 17:44:10 +0530

    introduce functions

---
+++ function.ts
@@ -0,0 +1,17 @@
+function addTwo(num: number) {
+     return num + 2;
+}
+
+function getUpper(val: string) {
+     return val.toUpperCase()
+}
+
+let loggedIn = (email: string, number: number, isPaid: boolean, name: string) => {
+     return "yes";
+}
+
+addTwo(5);
+getUpper("hello");
+loggedIn("harsh@gmail.com", 655, true, "hari")
+
+export { }
